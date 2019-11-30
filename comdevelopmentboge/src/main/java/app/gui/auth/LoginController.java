221dc55fc43e6a5244ea8a1f9cee62788cb42e51
SmiesnaA <47824795+SmiesnaA@users.noreply.github.com>
2019-11-30 20:27:38 +0100

    Add files via upload

--- comdevelopmentboge/src/main/java/app/gui/auth/LoginController.java
+++ comdevelopmentboge/src/main/java/app/gui/auth/LoginController.java
@@ -1,83 +1,71 @@
 package app.gui.auth;
 
-import app.App;
 import app.gui.home.HomeController;
+import app.gui.registration.RegistrationController;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController {
 
-    double x, y;
-    boolean fullSized;
-    Image eyeOn = new Image ("file:eyeOn.png");
-    Image eyeOff = new Image ("file:eyeOff.png");
-    boolean passwordShown = false;
+    boolean passwordShown;
+
+    Image eyeOn = new Image("app/images/eyeOn.png");
+    Image eyeOff = new Image("app/images/eyeOff.png");
+
     @FXML private ImageView eye;
+    @FXML private PasswordField passwordField;
+    @FXML private TextField passwordVisible;
 
-    @FXML
-    void dragged(MouseEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setX(event.getScreenX() - x);
-        stage.setY(event.getScreenY() - y);
-    }
-
-    @FXML
-    void pressed(MouseEvent event) {
-        x = event.getSceneX();
-        y = event.getSceneY();
-    }
-
-    @FXML
-    private void min(MouseEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setIconified(true);
-    }
-
-    @FXML
-    private void max(MouseEvent event) {
-        fullSized = !fullSized;
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        if(fullSized) {
-            stage.setMaximized(true);
-        }
-        else {
-            stage.setMaximized(false);
-        }
-
-    }
-
-    @FXML
-    private void close(MouseEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.close();
-    }
-
-
-    //zatial nefunguje
     @FXML
     private void showPassword(MouseEvent event) {
         passwordShown = !passwordShown;
         if(passwordShown) {
             eye.setImage(eyeOn);
+            passwordVisible.setText(passwordField.getText());
+            passwordVisible.setVisible(true);
+            passwordField.setVisible(false);
         }
         else {
             eye.setImage(eyeOff);
+            passwordField.setText(passwordVisible.getText());
+            passwordField.setVisible(true);
+            passwordVisible.setVisible(false);
         }
 
     }
 
 
     public void initialize() {
+        passwordShown = false;
+        eye.setImage(eyeOff);
+        passwordVisible.setVisible(false);
+        passwordField.setVisible(true);
+    }
 
+    //TO DO - nacitanie registracie
+    public void openRegistration(MouseEvent event) throws  IOException {
+        //RegistrationController.getRegistrationController().initialize();
     }
 
     public void openMainPage(MouseEvent event) throws IOException {
         HomeController.getHomeController().initialize();
     }
+
+
+    public void login(MouseEvent event) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, "Nesprávny tvar emailu", ButtonType.OK);
+        alert.showAndWait();
+        if (alert.getResult() == ButtonType.OK) {
+           alert.close();
+        }
+    }
+
 }
