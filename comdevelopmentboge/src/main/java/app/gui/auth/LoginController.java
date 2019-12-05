9bdba99df996c784dc78461990598cd05ebfe622
janaoravcova <janora78@gmail.com>
2019-12-05 21:16:20 +0100

    fix

--- comdevelopmentboge/src/main/java/app/gui/auth/LoginController.java
+++ comdevelopmentboge/src/main/java/app/gui/auth/LoginController.java
@@ -68,9 +68,6 @@ public class LoginController {
         if (alert.getResult() == ButtonType.OK) {
            alert.close();
         }
-
-        //do not commit
-        TabController.getInstance().init();
     }
 
 }
